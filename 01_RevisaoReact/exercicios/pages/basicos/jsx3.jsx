9611ccd5bb471e93c81c9ed904c4d9e87977d153
Andre Luis Kunert <seuemail@example.com>
2025-08-08 23:25:32 -0300

    Revisando Códigos ReactJS

--- 01_RevisaoReact/exercicios/pages/basicos/jsx3.jsx
+++ 01_RevisaoReact/exercicios/pages/basicos/jsx3.jsx
@@ -1,12 +1,7 @@
 export default function jsx3() {
-   return (
-      <div>
-        <h1> JXS #03</h1>
-        <div className="legal">LEGA</div>
-        <span id="so"></span>
-        <li className="apena"></li>
-      </div>
-   )
-
-
+    return (
+        <div>
+            <h1>JSX #03</h1>
+        </div>
+    )
 }
