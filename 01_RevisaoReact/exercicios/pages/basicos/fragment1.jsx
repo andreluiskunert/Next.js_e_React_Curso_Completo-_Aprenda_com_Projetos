7fce542f4694f8b4ca1afd863d89d7529dbcc9eb
Andre Luis Kunert <seuemail@example.com>
2025-08-11 16:38:34 -0300

    Bora codar Next/React.js

---
+++ 01_RevisaoReact/exercicios/pages/basicos/fragment1.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+export default function fragment(){
+    return (
+    <React.Fragment>
+        <h1>fragment1</h1>
+    </React.Fragment>
+
+    )
+}
---
+++ 01_RevisaoReact/exercicios/pages/basicos/fragment2.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+export default function fragment(){
+    return (
+    <>
+        <h1>fragment2</h1>
+        <h3> aqui é outro</h3>
+    </>
+
+    )
+}
