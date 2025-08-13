ffbc02e480984960f3930b76bd1214160b2c3f98
Andre Luis Kunert <seuemail@example.com>
2025-08-12 23:13:14 -0300

    Bora Codar com Next&React_2025

---
+++ 01_RevisaoReact/exercicios/pages/css/integracao1.jsx
@@ -0,0 +1,13 @@
+// Não posso importar aqui!
+// O import foi feito dentro de _app.js
+// import "./intregacao1.css" 
+
+export default function integracao(){
+     return (
+        <div>
+            <div className="vermelha">Desenvolvimento</div>
+            <div className="azul">Full</div>
+            <div className="green">Stack</div>
+        </div>
+     )
+}
