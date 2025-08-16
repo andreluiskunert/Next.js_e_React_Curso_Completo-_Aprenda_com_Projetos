07a22dcd0c5c2b6ea6ba5f253ad674d648aea5ab
Andre Luis Kunert <seuemail@example.com>
2025-08-16 16:15:01 -0300

    Bora Codando com Next/React.js

--- 01_RevisaoReact/exercicios/pages/render/condicional2.jsx
+++ 01_RevisaoReact/exercicios/pages/render/condicional2.jsx
@@ -3,13 +3,14 @@ import If from "../../components/If";
 export default function condicional1() {
     const numero = 5
     return (
-        <div>
-            <If teste={numero % 2 === 0}>
-                <h1>O número {numero} é par</h1>
-            </If>
-            <If teste={numero % 2 === 1}>
-                <h1>O número {numero} é ímpar</h1>
-            </If>
-        </div>
+        <>
+          <If teste ={numero % 2 === 0}>
+            O número {numero} é par...
+          </If>
+           <If teste ={numero % 2 === 1}>
+            O número {numero} é impar...
+          </If>
+        </>
+      
     )
 }
