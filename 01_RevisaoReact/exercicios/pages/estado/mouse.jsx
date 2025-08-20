6e54295bc4444c82f8c55ec2d8e224f022a81ad2
Andre Luis Kunert <seuemail@example.com>
2025-08-20 16:37:42 -0300

    Bora Codar em Next com React.js

--- 01_RevisaoReact/exercicios/pages/estado/mouse.jsx
+++ 01_RevisaoReact/exercicios/pages/estado/mouse.jsx
@@ -2,31 +2,35 @@ import { useState } from "react"
 
 export default function mouse() {
     // React Hooks
-    const [x, setX] = useState(0)
+  const [x, setX] = useState(0)
+  
+//  Y
+  const arrayY = useState(0)
+  let y = arrayY[0]
+  const alterarY = arrayY[1]
 
-    const arrayY = useState(0)
-    let y = arrayY[0]
-    const alterarY = arrayY[1]
-
+    
+ 
     const estilo = {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#222",
-        color: "#fff",
-        height: "100vh"
+      display:"flex",
+      flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+     backgroundColor: "#222",
+     color: "#FFFF",
+     height:"100vh"
     }
+    function quantoMover(ev){
+      setX(ev.clientX)
+     alterarY(ev.clientY)
 
-    function quandoMover(ev) {
-        setX(ev.clientX)
-        alterarY(ev.clientY)
+      console.log('Valor Alterado...')
     }
-
-    return (
-        <div style={estilo} onMouseMove={quandoMover}>
-            <span>Eixo X: {x}</span>
-            <span>Eixo Y: {y}</span>
-        </div>
-    )
+  return (
+   <div style={estilo}onMouseMove={quantoMover}>
+    <span> Eixo x : {x} </span>
+     <span> Eixo y : {y} </span>
+   </div>
+  )
+    
 }
