0ccc09389e1d4a653117c55b4e3d244ad397c618
Andre Luis Kunert <seuemail@example.com>
2025-08-21 23:12:39 -0300

    Bora Codar com Next/React.js_2025

--- 01_RevisaoReact/exercicios/pages/estado/formulario.jsx
+++ 01_RevisaoReact/exercicios/pages/estado/formulario.jsx
@@ -3,19 +3,18 @@ import { useState } from "react"
 export default function formulario() {
     const [valor, setValor] = useState("")
 
-    function alterarInput() {
+    function alterarInput(){
         setValor(valor + "!")
     }
-
-    return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column"
-        }}>
-            <span>{valor}</span>
-            <input type="text" value={valor}
-                onChange={e => setValor(e.target.value)} />
-            <button onClick={alterarInput}>Alterar</button>
-        </div>
-    )
+   return (
+    <div style={{
+          display: "flex",
+          flexDirection: "column"
+    }}>
+        <span>{valor} </span>
+        <input type="text" value={valor}
+          onChange={e => setValor(e.target.value)} />
+        <button onClick={alterarInput}> alterar </button>
+    </div>
+   )
 }
