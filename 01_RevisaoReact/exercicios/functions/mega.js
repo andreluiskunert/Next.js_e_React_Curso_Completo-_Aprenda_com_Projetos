c9a0a1585046f67bceb7fcc74c7b3d809a35f2ff
Andre Luis Kunert <seuemail@example.com>
2025-08-23 15:16:46 -0300

    Bora Codar com Next/ReactJS

--- 01_RevisaoReact/exercicios/functions/mega.js
+++ 01_RevisaoReact/exercicios/functions/mega.js
@@ -1,13 +1,16 @@
-export function mega(qtde = 6, numeros = []){
-    if(qtde < 6 && qtde > 60) {
-        throw "quantidade invalido"
+export function mega(quantidade = 6, numeros = []) {
+    let qtde = +quantidade
+    if (qtde < 6 && qtde > 60) {
+        throw "Quantidade inválida!"
     }
-    if(numeros.length === qtde){
-        return numeros.sort((n1, n2) => n1 - n2 )
+
+    if (numeros.length === qtde) {
+        return numeros.sort((n1, n2) => n1 - n2)
     }
-    const numeroAletorio = parseInt(Math.random() * 60) + 1
-    if(!numeros.includes(numeroAletorio)){
-        return mega(qtde, [...numeros, numeroAletorio])
+
+    const numeroAleatorio = parseInt(Math.random() * 60) + 1
+    if (!numeros.includes(numeroAleatorio)) {
+        return mega(qtde, [...numeros, numeroAleatorio])
     } else {
         return mega(qtde, numeros)
     }
--- 01_RevisaoReact/exercicios/pages/estado/megasena.jsx
+++ 01_RevisaoReact/exercicios/pages/estado/megasena.jsx
@@ -1,13 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import NumeroDisplay from "../../components/NumeroDisplay"
 import { mega } from "../../functions/mega"
-export default function megasena(){
-    const[numeros, setNumeros] = useState(mega())
-    function renderNumeros(){
-        return numeros.map(n => <NumeroDisplay key={numero} numero={numero}/>)
+
+export default function megasena() {
+
+    const [qtde, setQtde] = useState(6)
+    const [numeros, setNumeros] = useState([])
+
+    useEffect(() => {
+        setNumeros(mega())
+    }, [])
+
+    function renderizarNumeros() {
+        return numeros.map(
+            numero => <NumeroDisplay key={numero} numero={numero} />
+        )
     }
-    return(
-        <div>
-            <h1>Mega Sena </h1>
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+        }}>
+            <h1>Mega Sena</h1>
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                flexWrap: "wrap",
+            }}>
+                {renderizarNumeros()}
+            </div>
+            <div>
+                <input type="number" min={6} max={20} value={qtde}
+                    onChange={ev => setQtde(+ev.target.value)} />
+                <button onClick={() => setNumeros(mega(qtde))}>
+                    Gerar Aposta
+                </button>
+            </div>
         </div>
     )
 }
