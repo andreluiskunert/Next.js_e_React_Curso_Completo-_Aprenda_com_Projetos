390150ac54e45a280db467939c7a8a7457e1e130
Andre Luis Kunert <seuemail@example.com>
2025-08-24 00:37:42 -0300

    Bora codar com Next/Reactjs

---
+++ 01_RevisaoReact/exercicios/components/Contador.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Contador extends Component{
+    render(){
+        return(
+            <div>
+               <h1> Testando Classes no JSX </h1>
+            </div>
+        )
+    }
+}
