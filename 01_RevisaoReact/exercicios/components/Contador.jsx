6f9971abcc680cd2949f5f38c4f0d08b454fb102
Andre Luis Kunert <seuemail@example.com>
2025-08-25 16:49:50 -0300

    Bora Coda com Next/React.js

--- 01_RevisaoReact/exercicios/components/Contador.jsx
+++ 01_RevisaoReact/exercicios/components/Contador.jsx
@@ -3,14 +3,41 @@ import { Component } from "react";
 
 export default class Contador extends Component{
     state = {
-        numero : 7
+        numero : this.props.valorInicial ?? 0,
+        passo: this.props.passo ?? 1
+    }
+    // constructor(props) {
+    //     super(props)
+    //         this.inc = this.inc.bind(this)
+        
+    inc = () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo
+        })
+    }
+      dec = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo
+        })
+    }
+    renderForm(){
+        return(
+            <>
+              <input type="number" min={1} max={1000}value={this.state.passo} onChange={ev => this.setState({passo: +ev.target.value})} />
+              <button onClick={this.inc}> + </button>
+              <button onClick={this.dec}> - </button>
+            </>
+        )
     }
     render(){
         return(
             <div>
                 <h1>contador</h1>
-              <h2>{this.props.valorInicial} </h2> 
-              <h3>{this.state.numero}</h3>
+              <h2> Número inicial: {this.props.valorInicial} </h2> 
+              <h3> Número parcial:  {this.state.numero}</h3>
+             {this.renderForm()}
+
+
             </div>
         )
     }
