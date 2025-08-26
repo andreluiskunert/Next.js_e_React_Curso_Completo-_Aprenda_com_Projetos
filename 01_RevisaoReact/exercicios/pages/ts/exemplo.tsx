2ce0c87d2ac0299323ff3f646be6cb0e1d53b7e0
Andre Luis Kunert <seuemail@example.com>
2025-08-26 16:06:21 -0300

    Codando com Next/React.js_2025

--- 01_RevisaoReact/exercicios/pages/ts/exemplo.tsx
+++ 01_RevisaoReact/exercicios/pages/ts/exemplo.tsx
@@ -1,10 +1,10 @@
-import Pessoa from "../../components/Pessoa.tsx"
+// // import Pessoa from "../../components/Pessoa.tsx"
 
-export default function exemploTS() {
-  return (
-    <div>
-      <Pessoa nome="Joao" idade={30} />
-      <Pessoa nome="Maria" idade={25} />
-    </div>
-  )
-}
+// export default function exemploTS() {
+//   return (
+//     <div>
+//       <Pessoa nome="Joao" idade={30} />
+//       <Pessoa nome="Maria" idade={25} />
+//     </div>
+//   )
+// }
