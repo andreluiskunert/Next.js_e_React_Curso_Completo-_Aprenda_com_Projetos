56c281c3524cf312590a6eb6c74a3bdf142db24c
Andre Luis Kunert <seuemail@example.com>
2025-09-01 23:08:52 -0300

    Codando com next/react.js_2025

--- 3_Projeto_Porta_Premiada_Aplicando_os_Conceitos_Fundamentais/portas/components/Presente.jsx
+++ 3_Projeto_Porta_Premiada_Aplicando_os_Conceitos_Fundamentais/portas/components/Presente.jsx
@@ -2,8 +2,8 @@ import style from "../styles/Presente.module.css"
 export default function Presente(){
     return(
         <div className={style.presente}>
-          <div className={style.tampa}>Tampa</div>
-          <div className={style.corpo}>Corpo</div>
+          <div className={style.tampa}></div>
+          <div className={style.corpo}></div>
         </div>
     )
 }
