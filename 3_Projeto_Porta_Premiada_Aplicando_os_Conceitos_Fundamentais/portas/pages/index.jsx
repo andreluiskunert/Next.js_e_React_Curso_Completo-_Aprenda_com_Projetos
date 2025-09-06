fd3b570e2d9a3703b5f551103dde0c23cad8cc37
Andre Luis Kunert <seuemail@example.com>
2025-09-05 23:25:31 -0300

    Bora Codar com Next/React.js_2025

---
+++ 3_Projeto_Porta_Premiada_Aplicando_os_Conceitos_Fundamentais/portas/pages/index.jsx
@@ -0,0 +1,11 @@
+import Porta from "../components/Porta"
+
+export default function Home() {
+    return (
+        <div style={{display: "flex"}} >
+        <Porta/>
+        <Porta/>
+        <Porta/>
+        </div>
+    )
+}
