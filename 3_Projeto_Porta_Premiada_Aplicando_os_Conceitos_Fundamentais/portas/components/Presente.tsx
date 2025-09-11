2fa8e3465e5979dff9aa82889a5e2e788e60a1b9
Andre Luis Kunert <seuemail@example.com>
2025-09-10 23:33:06 -0300

    bora codar com Next/React.js_2025

--- 3_Projeto_Porta_Premiada_Aplicando_os_Conceitos_Fundamentais/portas/components/Presente.tsx
+++ 3_Projeto_Porta_Premiada_Aplicando_os_Conceitos_Fundamentais/portas/components/Presente.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/Presente.module.css"
 
-export default function Presente(){
+export default function Presente() {
     return (
         <div className={styles.presente}>
             <div className={styles.tampa}></div>
