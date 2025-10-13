ca73bace3a0a55b0665083353563dcdffbed710c
Andre Luis Kunert <seuemail@example.com>
2025-10-12 22:56:49 -0300

    Bora Codar com Next/React.js_2025

--- 4_Modo_Pre_Renderizacao_do_Next.js/renderizacao/pages/api/alunos/[id].js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    const id = +req.query.id
-    res.status(200).json({
-        id,
-        nome: `João Almeida ${id}`,
-        email: `jjjalalalmmm${id}@gmail.com`
-    })
-}
