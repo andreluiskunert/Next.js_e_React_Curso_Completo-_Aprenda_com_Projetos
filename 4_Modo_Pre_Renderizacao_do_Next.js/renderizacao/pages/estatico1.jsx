0152ea70afea94fc44fad978e5ce5bb4434cb2a3
Andre Luis Kunert <seuemail@example.com>
2025-10-13 23:25:38 -0300

    Bora Codar Next/React.js_2025

---
+++ 4_Modo_Pre_Renderizacao_do_Next.js/renderizacao/pages/estatico1.jsx
@@ -0,0 +1,14 @@
+export default function Estatico1() {
+    // const [num, setNum] = useState(0)
+
+    // useEffect(() => {
+    //     setNum(Math.random())
+    // }, [])
+
+    return (
+        <div>
+            <h1>Estático #01</h1>
+            {/* <h2>{num}</h2> */}
+        </div>
+    )
+}
