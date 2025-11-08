d9ee8f86af2caed44256a3c5db90751e99d9a839
Andre Luis Kunert <seuemail@example.com>
2025-11-08 11:44:43 -0300

    bora Codar com Next/React.js_2025

--- 5_Porjeto_Quiz_Full_Stack_com_Next.js/perguntas_respostas/pages/api/questionario/index.ts
+++ 5_Porjeto_Quiz_Full_Stack_com_Next.js/perguntas_respostas/pages/api/questionario/index.ts
@@ -1,7 +1,8 @@
+import { Request, Response } from 'express'
 import { embaralhar } from '../../../functions/arrays'
 import questoes from '../bancoDeQuestoes'
 
-export default function questionario(req, res) {
-    const ids = questoes.map(questao => questao.id)
-    res.status(200).json(embaralhar(ids))
+export default function questionario(req: Request, res: Response) {
+  const ids = questoes.map((questao) => questao.id)
+  res.status(200).json(embaralhar(ids))
 }
