8a6140f7ca5e251d0bc89b22b096e7b14bdc585f
Andre Luis Kunert <seuemail@example.com>
2025-11-11 22:44:19 -0300

    bora codar com Next/React.js_2025

--- 5_Porjeto_Quiz_Full_Stack_com_Next.js/perguntas_respostas/model/questao.ts
+++ 5_Porjeto_Quiz_Full_Stack_com_Next.js/perguntas_respostas/model/questao.ts
@@ -41,14 +41,14 @@ export default class QuestaoModel {
         return false
     }
 
-    responderCom(indice: number): QuestaoModel {
-        const acertou = this.#respostas[indice]?.certa
+    responderCom(indice: number): QuestaoModel { const acertou = this.#respostas[indice]?.certa
         const respostas = this.#respostas.map((resposta, i) => {
             const respostaSelecionada = indice === i
             const deveRevelar = respostaSelecionada || resposta.certa
             return deveRevelar ? resposta.revelar() : resposta
         })
         return new QuestaoModel(this.id, this.enunciado, respostas, acertou)
+
     }
 
     embaralharRespostas(): QuestaoModel {
