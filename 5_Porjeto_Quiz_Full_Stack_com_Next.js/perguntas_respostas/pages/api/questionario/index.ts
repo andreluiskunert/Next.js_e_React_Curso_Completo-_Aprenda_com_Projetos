77f8b500f6d20429570503efbaba901f1517907b
Andre Luis Kunert <58589369+andreluiskunert@users.noreply.github.com>
2025-11-15 12:07:23 -0300

    Delete 5_Porjeto_Quiz_Full_Stack_com_Next.js directory
    Irei fazer novamente do zero

--- 5_Porjeto_Quiz_Full_Stack_com_Next.js/perguntas_respostas/pages/api/questionario/index.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express'
-import { embaralhar } from '../../../functions/arrays'
-import questoes from '../bancoDeQuestoes'
-
-export default function questionario(req: Request, res: Response) {
-  const ids = questoes.map((questao) => questao.id)
-  res.status(200).json(embaralhar(ids))
-}
