497b025dc7191bf9bb42e6bc2baf31de4299cd5d
Andre Luis Kunert <seuemail@example.com>
2025-12-30 17:31:16 -0300

    bora la codar com next/react.js_2025

---
+++ 5_Projeto_Quiz_Full_Stack_com_NextJS/quiz/model/questao.ts
@@ -0,0 +1,8 @@
+export default class QuestaoModel {
+    '#id': number
+    '#enunciado': string
+    '#respostas': any[]
+    '#acertou': boolean
+    // '#respondida':boolean
+//  de noite continuou.....
+}
